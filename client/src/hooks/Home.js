1afa5b7ec1a61657bc6df1d3fd9f86aaa68df7e9
Jimmy in Rain <quangminhit94@gmail.com>
2020-08-18 14:21:35 +0700

    Add theme

--- client/src/hooks/Home.js
+++ client/src/hooks/Home.js
@@ -1,34 +1,71 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import Axios from 'axios'
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types'
 import Context from '../utils/context'
+import ThemeContext from '../utils/ThemeContext'
 import LoaderHOC from '../HOC/LoaderHOC'
 import Button from '@material-ui/core/Button'
-// import history from '../utils/history'
 
+const Slider = () => {
+  const { themeState, toggleTheme } = useContext(ThemeContext)
 
+  console.log(themeState)
+  return (
+    <label className="switch">
+      <input
+        onChange={toggleTheme}
+        type="checkbox"
+        checked={themeState.theme === "night"}
+      />
+      <span className="slider round" />
+    </label>
+  )
+}
+const Footer = () => {
+
+  return (
+    <footer>
+      <label>night theme:</label> <Slider />
+    </footer>
+  )
+};
 
 const Home = ({textHome}) => {
   const context = useContext(Context)
+  // const initialThemeState = themeContext
+  const [themeState, setThemeState] = useState({theme: 'night'})
 
+  const toggleTheme = evt => {
+    console.log(evt.target.checked)
+    setThemeState({ theme: evt.target.checked ? "night" : "day" });
+    console.log(evt.target.checked)
+    console.log('themeState', themeState)
+  };
+  
   return (
     <div>
-      Home: {textHome}
-      <br/>
-      <br/>
-      {!localStorage.user_id
-        ? <Link style={{ paddingLeft: '5px', textDecoration: 'none' }}
-            to={{ pathname: "/login"}}>
-            <Button color='secondary' variant='outlined'>Login</Button>
-          </Link>
-         : <div>
-            <Link style={{ paddingLeft: '5px', textDecoration: 'none' }}
-                to={{ pathname: "/profile1/" + localStorage.user_id }}>
-                Hello {localStorage.user_id} </Link> 
-            <Button color='secondary' variant='outlined' onClick={() => context.logout()}>Logout</Button>
-          </div>
-      }
+      {/* Home: {textHome} */}
+      
+      <ThemeContext.Provider value={{themeState, toggleTheme}}>
+        <div className={themeState.theme}>
+          {!localStorage.user_id
+            ? <Link style={{ paddingLeft: '5px', textDecoration: 'none' }}
+                to={{ pathname: "/login"}}>
+                <Button color='secondary' variant='outlined'>Login</Button>
+              </Link>
+            : <div>
+                <Link style={{ paddingLeft: '5px', textDecoration: 'none' }}
+                    to={{ pathname: "/profile1/" + localStorage.user_id }}>
+                    Hello {localStorage.user_id} </Link> 
+                <Button color='secondary' variant='outlined' onClick={() => context.logout()}>Logout</Button>
+              </div>
+          }
+          
+          <Footer />
+        </div>
+      </ThemeContext.Provider>
+      
     </div>
   )
 }
--- client/src/utils/context.js
+++ client/src/utils/context.js
@@ -1,7 +1,15 @@
 import { createContext } from 'react'
+const themes = {
+  light: {
+    foreground: "#000000",
+    background: "#eeeeee"
+  },
+  dark: {
+    foreground: "#ffffff",
+    background: "#222222"
+  }
+};
 
-const Context =  createContext({
-  prop1: false
-})
+const Context =  createContext(themes)
 
 export default Context
